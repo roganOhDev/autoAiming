f29138f77280fc7954cf90026837aa4091341d85
roganOh <rogan@chequer.io>
2022-09-14 20:04:05 +0900

    sub

--- main.py
+++ main.py
@@ -1,54 +1,50 @@
-import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 import imutils
 
 filename = "full_body13.png"
-# 예측할 그림 가져오기, gray convert
 img = cv2.imread(filename)
+# get img
 
 hog = cv2.HOGDescriptor()
+# get HOG algorithm
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+# input data in svm with HOG algorithm which detecting people with cv
 
 img = imutils.resize(img,
                      width=min(400, img.shape[1]))
+# resize img 400 or img.size whichever is smaller
 
 (regions, _) = hog.detectMultiScale(img,
                                     winStride=(4, 4),
                                     padding=(4, 4),
                                     scale=1.05)
+#  detect mutli people from img. with winStride(4,4),
+#  winStride : 2-tuple that dictats the step size
+#  padding : surround the object
 
 for (x, y, w, h) in regions:
+    # for each human
+
     cv2.rectangle(img, (x, y),
                   (x + w, y + h),
                   (0, 0, 255), 2)
+    # draw rectangle
 
-    half_of_x = x+round(w*0.5)
-    half_of_y = y+round(h*0.35)
+    half_of_x = x + round(w * 0.5)
+    half_of_y = y + round(h * 0.35)
 
     img = cv2.line(img, (half_of_x, y), (half_of_x, y + h), (0, 0, 255), 1)
-    img = cv2.line(img, (x, half_of_y), (x+w, half_of_y), (0, 0, 255), 1)
+    img = cv2.line(img, (x, half_of_y), (x + w, half_of_y), (0, 0, 255), 1)
+    # draw line
 
 cv2.imshow('frame', img)
-while (True):
+#  show frame
+
+while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+        # wait key 'q'
+
 cv2.destroyAllWindows()
-#
-# body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
-#
-# while(True):
-#     frame = image1
-#     grayImage1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-#     body = body_cascade.detectMultiScale(grayImage1, 1.01, 1)
-#
-#
-#     for (x, y, w, h) in body:
-#         cv2.rectangle(image1, (x, y), (x + w, y + h), (0, 0, 255), 3)
-#
-#     cv2.imshow('frame', frame)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# cv2.destroyAllWindows()
+# destroy all windows
